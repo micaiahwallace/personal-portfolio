885a68841d919a982cfdb9813db656f81318f75d
Micaiah Wallace <micaiah@snoapps.com>
2023-05-08 23:16:26 -0400

    add basic social icons. portfolio item 1. resume embeded.

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,20 +1,34 @@
 <script>
 	import { page } from '$app/stores';
 	import github from '$lib/images/github.svg';
+	import linkedin from '$lib/images/linkedin.svg';
+	import medium from '$lib/images/medium.svg';
 </script>
 
 <header>
 	<nav>
 		<ul>
 			<li aria-current={$page.url.pathname === '/projects' ? 'page' : undefined}>
-				<a href="/">Projects</a>
+				<a href="/projects">Projects</a>
 			</li>
 			<li aria-current={$page.url.pathname === '/resume' ? 'page' : undefined}>
-				<a href="/">Résumé</a>
+				<a href="/resume">Résumé</a>
 			</li>
+		</ul>
+		<ul>
 			<li>
 				<a href="https://github.com/micaiahwallace">
 					<img src={github} alt="GitHub" />
+				</a>
+			</li>
+			<li>
+				<a href="https://www.linkedin.com/in/micaiahwallace/">
+					<img src={linkedin} alt="LinkedIn" />
+				</a>
+			</li>
+			<li>
+				<a href="https://micaiah-wallace.medium.com/">
+					<img src={medium} alt="Medium" />
 				</a>
 			</li>
 		</ul>
@@ -28,16 +42,21 @@
 	}
 
 	nav {
+		width: 100%;
 		display: flex;
-		justify-content: center;
+		justify-content: space-between;
 		--background: rgba(255, 255, 255, 0.7);
+	}
+
+	img {
+		height: 2em;
 	}
 
 	ul {
 		position: relative;
 		padding: 0;
 		margin: 0;
-		height: 3em;
+		height: 4em;
 		display: flex;
 		justify-content: center;
 		align-items: center;
@@ -63,7 +82,7 @@
 		border-top: var(--size) solid var(--color-theme-1);
 	}
 
-	nav a {
+	a {
 		display: flex;
 		height: 100%;
 		align-items: center;
@@ -77,7 +96,22 @@
 		transition: color 0.2s linear;
 	}
 
-	a:hover {
-		color: var(--color-theme-1);
+	a::before {
+		content: '';
+		position: absolute;
+		width: 100%;
+		height: 4px;
+		border-radius: 4px;
+		background-color: #18272f;
+		bottom: 0;
+		left: 0;
+		transform-origin: right;
+		transform: scaleX(0);
+		transition: transform 0.2s ease-in-out;
+	}
+
+	a:hover::before {
+		transform-origin: left;
+		transform: scaleX(1);
 	}
 </style>
---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import PdfViewer from 'svelte-pdf';
+</script>
+
+<div>
+	<PdfViewer url="/Micaiah_Wallace_Senior_Software_Engineer.pdf" data={undefined} />
+</div>
