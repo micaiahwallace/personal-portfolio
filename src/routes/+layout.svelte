2f2ac37a9ce9f8b0f9bab227c8f8b5b540a3aea3
Micaiah Wallace <micaiah@snoapps.com>
2026-01-17 08:21:10 -0800

    add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
 	import './styles.css';
-	import 'prism-themes/themes/prism-gruvbox-dark.css'
+	import 'prism-themes/themes/prism-gruvbox-dark.css';
 
 	inject({ mode: dev ? 'development' : 'production' });
 </script>
@@ -11,7 +11,14 @@
 <svelte:head>
 	<title>Micaiah Wallace</title>
 	<meta name="description" content="Micaiah Wallace - Portfolio" />
-	<meta name="viewport" content= "width=device-width, user-scalable=no">
+	<meta
+		name="viewport"
+		content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+	/>
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+	<link rel="manifest" href="/site.webmanifest" />
 </svelte:head>
 
 <div class="app">
