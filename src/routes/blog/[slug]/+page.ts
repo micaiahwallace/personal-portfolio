d78a823a2bfa6910ba27604d98407366c8239f66
Micaiah Wallace <micaiah@snoapps.com>
2026-01-16 22:36:10 -0800

    Fix missing slug route

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,7 +1,15 @@
-import { getPost } from '$lib/posts';
+import { getPost, getAllPosts } from '$lib/posts';
 import { error } from '@sveltejs/kit';
 
 export const prerender = true;
+
+export async function entries() {
+	const posts = await getAllPosts();
+
+	return posts.map((post) => ({
+		slug: post.slug
+	}));
+}
 
 export async function load({ params }) {
 	const post = await getPost(params.slug);
